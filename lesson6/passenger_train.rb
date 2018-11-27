3ec4ddb4d97d0b4751c71b739ade72cd5e89a97a
Part1nax777 <serewka-ru@yandex.ru>
2018-11-27 17:28:01 +0300

    lesson6

--- lesson6/passenger_train.rb
+++
@@ -1,6 +0,0 @@
-class PassengerTrain < Train
-
-   def attachable_wagon?(wagon)
-    wagon.is_a?(PassengerWagon)
-   end
-end
--- lesson6/wagon.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'company_name'
-
-class Wagon
-  include CompanyName
-end
