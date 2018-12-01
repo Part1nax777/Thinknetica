862685b4b863ef8a4226ed04e7414d148fe73212
Part1nax777 <serewka-ru@yandex.ru>
2018-12-01 20:19:03 +0300

    lesson7 fix

---
+++ lesson7/passenger_train.rb
@@ -0,0 +1,12 @@
+class PassengerTrain < Train
+  attr_reader :type
+
+  def initialize(number)
+    super
+    @type = :passenger
+  end
+  
+  def attachable_wagon?(wagon)
+    wagon.is_a?(PassengerWagon)
+  end
+end
